2fddf49ee32403b519ddb472ab05f19945cda813
Simone C <simone.cuffaro@air-srl.com>
2022-09-16 11:10:24 +0200

    [INIT] modulo esempio per Marinor

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Marinor custom',
+    'version': '14.0',
+    'description': 'Customizzazioni Marinor',
+    'author': 'SC',
+    'license': 'OPL-1',
+    'category': "Extra Tools",
+    'depends': ['base',
+                'contacts'],
+    'data': [
+        'views/res_partner.xml'
+    ],
+    'installable': True,
+    'images': ['static/description/icon.png']
+}
