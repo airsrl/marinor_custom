c6f9ec35eb377033ce72ee799a9f9a632702c644
air-mattia <maf@air.co.it>
2022-11-23 09:35:28 +0100

    [UPD]Aggiunto campo agenti in fattura sconto calc

--- __manifest__.py
+++ __manifest__.py
@@ -3,15 +3,14 @@
     'name': 'Marinor custom',
     'version': '14.0',
     'description': 'Customizzazioni Marinor',
-    'author': 'SC',
+    'author': 'MF',
     'license': 'OPL-1',
     'category': "Extra Tools",
     'depends': ['base',
                 'contacts',
                 'sale_commission'],
     'data': [
-        'views/res_partner.xml',
-        'views/account_move.xml',
+
         'views/account_move_report.xml',
 
     ],
--- models/account_move.py
+++ models/account_move.py
@@ -4,10 +4,13 @@ from odoo import fields, models, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    # agenti = fields.Char(related='partner_agent_ids.display_name', string="agenti")
+    # price_lst = fields.Float(related="", string="Prezzo listino")
+    # calc_disc = fields.Float(compute="", string="Sconto Calcolato")
 
-    agenti = fields.Char(compute="_compute_agents_name", string="agenti")
+    # @api.depends("invoice_line_ids")
+    # def _compute_agents_name(self):
+    #     for record in self:
+    #             record.agenti = record.partner_agent_ids.display_name
 
-    @api.depends("invoice_line_ids")
-    def _compute_agents_name(self):
-        for record in self:
-                record.agenti = record.partner_agent_ids.display_name
+    
